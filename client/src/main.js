aa8c76fb9a9f449c668a97d8a83ce020edb8c57a
HuaRongSAO <qq1114282509@gmail.com>
2018-06-25 11:10:49 +0800

    Fixs

--- client/src/main.js
+++ client/src/main.js
@@ -5,14 +5,16 @@ import './api'
 import 'amfe-flexible/index.min.js'
 import store from './store'
 import vueg from 'vueg'
+import VueQrcode from '@xkeshi/vue-qrcode'
 import { Toast, Button, Switch, Swipe, SwipeItem, InfiniteScroll } from 'mint-ui'
-import VueQrcode from '@xkeshi/vue-qrcode';
+
 Vue.use(InfiniteScroll)
 Vue.component(Swipe.name, Swipe)
 Vue.component(SwipeItem.name, SwipeItem)
 Vue.component(Switch.name, Switch)
 Vue.component(Button.name, Button)
-Vue.component(VueQrcode.name, VueQrcode);
+Vue.component(VueQrcode.name, VueQrcode)
+
 Vue.config.productionTip = false
 Vue.use(vueg, router)
 Vue.use(store)
--- client/src/views/Course/Course.vue
+++ client/src/views/Course/Course.vue
@@ -1,16 +1,43 @@
 <template>
-  <div class="page">
+  <div class="page course">
     <div class="banner-top">
       <img src="./../../assets/banner1.jpeg">
+    </div>
+    <div class="course-time">
+      <div class="course-time__prev">< 上周</div>
+      <div class="course-time__next">下周 ></div>
+    </div>
+    <div class="course-day">
+      <div class="course-day__tabs">
+        <div class="course-day__tab" v-for="week in weekdays" :class="{active: week===active}"
+             :key="week + 'tabs'" @click="changeWeek(week)">{{week}}
+        </div>
+      </div>
+      <div class="course-day__content" v-for="week in weekdays" :key="week + 'content'"
+           :class="{active: week===active}">
+        <div class="course-day__article" v-for="i in 10" :key="week + 'content' +i ">
+          {{week + ':'+ i}}
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-  import './Course.less'
 
   export default {
-    name: 'Course'
+    name: 'Course',
+    data () {
+      return {
+        active: '周一',
+        weekdays: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+      }
+    },
+    methods: {
+      changeWeek (week) {
+        this.active = week
+      }
+    },
   }
 </script>
 <style lang="less">
@@ -18,6 +45,51 @@
     img {
       display: inline-block;
       width: 100%;
+    }
+  }
+
+  .course {
+    &-time {
+      display: flex;
+      padding: 20px 0;
+      & > div {
+        flex: 1;
+      }
+      &__prev {
+        text-align: left;
+      }
+      &__next {
+        text-align: right;
+      }
+    }
+    &-day {
+      &__tabs {
+        display: flex;
+      }
+      &__tab {
+        flex: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 28px;
+        &.active {
+          background: #eee;
+        }
+      }
+      &__content {
+        padding: 20px;
+        display: none;
+        &.active {
+          display: block;
+          transition: all 0.4s;
+        }
+      }
+      &__article {
+        height: 220px;
+        background: #eee;
+        border-radius: 5px;
+        margin-bottom: 20px;
+      }
     }
   }
 </style>
